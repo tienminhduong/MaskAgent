7f2cde8f81e2c75220ac0f3014a691a1e25b47a9
Tiền Minh Dương <tmd31052005@gmail.com>
2026-01-31 16:38:36 +0700

    Testing npc's eyesight

--- Assets/Scripts/Interactable/CharacterInteractLogic.cs
+++ Assets/Scripts/Interactable/CharacterInteractLogic.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class CharacterInteractLogic : MonoBehaviour
 {
+    [SerializeField] private IInteractable overlappedInteractable;
 
+    public void SetOverlappedInteractable(IInteractable interactable)
+    {
+        overlappedInteractable = interactable;
+        Debug.Log("I see you");
+    }
+
+    public void RemoveOverlappedInteractable(IInteractable interactable)
+    {
+        if (overlappedInteractable == interactable)
+        {
+            overlappedInteractable = null;
+        }
+    }
+
+    public void Interact()
+    {
+        Debug.Log($"Interacting with {overlappedInteractable}");
+        overlappedInteractable?.Interacted(overlappedInteractable);
+    }
 }
